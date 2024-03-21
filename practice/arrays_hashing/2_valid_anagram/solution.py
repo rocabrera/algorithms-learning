eabd0e40ab8c580f0b96d0b0fe74b7c9e34d4c8c
Rodrigo Castaldoni <castaldoniro@gmail.com>
2024-03-20 23:52:01 -0300

    feat: add internet solution
    Signed-off-by: Rodrigo Castaldoni <castaldoniro@gmail.com>

--- practice/arrays_hashing/2_valid_anagram/solution.py
+++ practice/arrays_hashing/2_valid_anagram/solution.py
@@ -35,3 +35,24 @@ class Solution:
             return False
 
         return True
+
+# from collections import defaultdict
+
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         count = defaultdict(int)
+        
+#         # Count the frequency of characters in string s
+#         for x in s:
+#             count[x] += 1
+        
+#         # Decrement the frequency of characters in string t
+#         for x in t:
+#             count[x] -= 1
+        
+#         # Check if any character has non-zero frequency
+#         for val in count.values():
+#             if val != 0:
+#                 return False
+        
+#         return True
