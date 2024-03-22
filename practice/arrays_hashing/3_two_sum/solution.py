1b3a06fb4064ae2865f4b947957159919370d373
Rodrigo Castaldoni <castaldoniro@gmail.com>
2024-03-21 22:21:45 -0300

    feat: add no solution found case
    Signed-off-by: Rodrigo Castaldoni <castaldoniro@gmail.com>

--- practice/arrays_hashing/3_two_sum/solution.py
+++ practice/arrays_hashing/3_two_sum/solution.py
@@ -16,9 +16,11 @@ class Solution:
 
         for idx, num in enumerate(nums):
             complement = target - num
-            
+
             if complement in hashmap:
                 return [idx, hashmap[complement]]
             
             if num not in hashmap:
                 hashmap[num] = idx
+
+        return []
