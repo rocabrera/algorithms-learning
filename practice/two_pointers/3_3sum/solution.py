7a00393e902923358b3e4f0b18b2984e52a99d77
Rodrigo Castaldoni <castaldoniro@gmail.com>
2024-04-13 18:03:32 -0300

    feat: 3sum
    Signed-off-by: Rodrigo Castaldoni <castaldoniro@gmail.com>

---
+++ practice/two_pointers/3_3sum/solution.py
@@ -0,0 +1,38 @@
+"""
+https://leetcode.com/problems/3sum/description/
+----
+Given an integer array nums, return all the triplets
+[nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k,
+and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+"""
+
+from typing import List
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+
+        nums = sorted(nums)
+        triplets = []
+        for i, num in enumerate(nums):
+            if i > 0 and num == nums[i-1]:
+                continue
+            l, r = i+1, len(nums) - 1
+            while r > l:
+                threeSum = num + nums[l] + nums[r]
+                if  threeSum == 0:
+                    triplets.append([num, nums[l], nums[r]]) 
+                    l += 1
+                    while r > l and nums[l] == nums[l-1]:
+                        l+=1
+                elif threeSum > 0:
+                    r -= 1
+                else:
+                    l += 1
+            
+        return triplets
+
+nums = [-1,0,1,0]
+results = Solution().threeSum(nums)
+print(results)
